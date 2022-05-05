70e7c0dbacbbe424d75e0d1586b299881b353260
Jorge García González <jorgegarcia.java@gmail.com>
2022-05-05 09:55:47 +0200

    Cloudinary

--- controllers/upload/upload.js
+++ controllers/upload/upload.js
@@ -1,7 +1,14 @@
-
+const { v4: uuid } = require('uuid');
+const cloudinary = require('cloudinary');
+cloudinary.config({
+   cloud_name: 'app-veterinary',
+   api_key: '887253888763596',
+   api_secret:'EfqZ_HpTIAtfy6a1ao-vpRRTU4Y'
+});
 
 const fileUpload = async (req,res) =>{
 
+    //Comprobamos si tiene permisos
     if(req.user.rol === 'USER_ROLE'){
         return res.status(401).json({
             ok: false,
@@ -14,8 +21,9 @@ const fileUpload = async (req,res) =>{
         const id = req.params.id;
         const model = req.params.model;
 
-        const modelsValids = ['user', 'aids', 'treatment', 'service', 'queries',
-            'promotion', 'product', 'pet', 'breed'];
+        //Comprobamos si es un modelo de datos válido
+        const modelsValids = ['user', 'aids', 'queries',
+            'promotion', 'product', 'pet'];
         if(!modelsValids.includes(model)){
             return res.status(401).json({
                 ok: false,
@@ -23,6 +31,7 @@ const fileUpload = async (req,res) =>{
             });
         }
 
+        //Comprobamos si hay un archivo en la petición
         if( !req.file || Object.keys(req.file).length === 0){
             return res.status(401).json({
                 ok: false,
@@ -30,10 +39,40 @@ const fileUpload = async (req,res) =>{
             });
         }
 
+        const file = req.files.file;
+        const parts = file.name.split('.');
+        const ext = parts[parts.length - 1];
 
-        res.status(200).json({
-            ok: true
-        });
+        //Comprobamos si es una ext válida
+        const extValid = ['png', 'jpg', 'jpeg', 'gif'];
+        if(!extValid.includes(ext)){
+            return res.status(401).json({
+                ok: false,
+                msg: 'Extensión no permitida'
+            });
+        }
+
+        //Generar nombre y path
+        const nameFile = `${uuid()}.${ext}`;
+        const path = `./files/${nameFile}`;
+
+        //Mover img
+        file.mv(path, async (err) =>{
+            if(err){
+                return res.status(500).json({
+                    ok: false,
+                    msg: "Error inesperado..., llame a su administrador"
+                });
+            }
+
+            const result = await cloudinary.v2.uploader.upload(path);
+
+            res.status(200).json({
+                ok: true,
+                msg: 'Archivo subido',
+                data: result.public_id
+            });
+        })
 
     }catch (error) {
         res.status(500).json({
--- routes/upload/upload.js
+++ routes/upload/upload.js
@@ -1,9 +1,12 @@
 const {Router} = require("express");
 const {fileUpload} = require('../../controllers/upload/upload');
 const {checkJWT} = require("../../middlewares/check-JWT");
+const expressFileUpload = require('express-fileupload');
 
 
 const router = Router();
+
+router.use(expressFileUpload());
 
 router.put('/:model/:id',
     [
